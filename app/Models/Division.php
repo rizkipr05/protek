5867535e4f40d9d19745d93b3da818ae11e22e25
rizkipr05 <rizki.qq05@gmail.com>
2025-10-08 16:17:08 +0700

    benerin eror pada tabel Divisions

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Division extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'code',
+    ];
+
+    // relasi opsional
+    public function users()
+    {
+        return $this->hasMany(\App\Models\User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(\App\Models\Task::class);
+    }
 }
